35f90a9f53c35f026ce1e2ee70c74ee3eae5db79
Piotr Sacharuk <107190444+PiotrSacharuk@users.noreply.github.com>
2025-12-27 22:40:38 +0100

    refactor: file structure into src dir (#3)

---
+++ src/core/Logger.cpp
@@ -0,0 +1,22 @@
+#include "Logger.h"
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+
+void Logger::log(const std::string& message, const std::string& logLevel) {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+    
+    std::ofstream logFile("system.log", std::ios::app);
+    
+    if (logFile.is_open()) {
+        logFile << "[" << std::put_time(std::localtime(&now_c), "%Y-%m-%d %H:%M:%S")
+            << "] " << "[" << logLevel << "] " << message << std::endl;
+    }
+}
+
+void Logger::logError(const std::string& message) {
+    log(message, "ERROR");
+}
